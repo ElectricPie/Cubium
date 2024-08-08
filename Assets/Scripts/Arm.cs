b4d13df7484e7fb9d1691e962fb561a41c0cecc4
Connor Priest <Kon_Priest@Hotmail.com>
2024-08-08 19:24:46 +0100

    Created arm for the camera

---
+++ Assets/Scripts/Arm.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Cubium
+{
+    public class Arm : MonoBehaviour
+    {
+        [SerializeField] [Min(0)] private float m_armLength = 2.0f;
+
+        public void SetArmLength(float newLength)
+        {
+            m_armLength = newLength;
+        }
+        
+        
+        private void OnValidate()
+        {
+            UpdateChildPositions();
+        }
+        
+        private void UpdateChildPositions()
+        {
+            // Set the distance of the arm to the specified value
+            foreach (Transform child in transform)
+            {
+                child.localPosition = new Vector3(0, 0, -m_armLength);
+            }
+        }
+    }
+}
