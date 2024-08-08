f1324ca4a0da1ff620b5b72a126e915a958f4d45
Connor Priest <Kon_Priest@Hotmail.com>
2024-08-08 18:10:13 +0100

    Setup input system

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Cubium.Player
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        public void Move(InputAction.CallbackContext context)
+        {
+            Vector2 move = context.ReadValue<Vector2>();
+            Debug.Log($"Move: {move}");
+        }
+    }
+}
