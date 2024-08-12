9e8f418975218ba7b184e1962079f24cb12587df
Connor <Kon_Priest@Hotmail.com>
2024-08-12 23:44:12 +0100

    Created gather resource component and tree

---
+++ Assets/Scripts/Resources/GatherableResource.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Cubium.Resources
+{
+    public class GatherableResource : MonoBehaviour
+    {
+        [SerializeField]
+        private ResourceType m_resourceType = null;
+        
+        [SerializeField]
+        private int m_gatherableAmount = 20;
+
+        public int Gather(int amount)
+        {
+            if (m_gatherableAmount - amount < 0)
+            {
+                Destroy(gameObject);
+                return m_gatherableAmount;
+            }
+            
+            m_gatherableAmount -= amount;
+            return amount;
+        }
+    }
+}
