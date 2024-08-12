1876d2d1e49895a3ecca417f5c3f1c5c64477c41
Connor <Kon_Priest@Hotmail.com>
2024-08-12 22:54:22 +0100

    Created scriptable object for resources

---
+++ Assets/Scripts/Resources/ResourceType.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Cubium.Resources
+{
+    [CreateAssetMenu(fileName = "New Resource", menuName = "Cubium/New Resource", order = 0)]
+    public class ResourceType : ScriptableObject
+    {
+        [SerializeField] private string Name;
+        [SerializeField] public Sprite Icon;
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public Sprite GetSprite()
+        {
+            return Icon;
+        }
+    }
+}
